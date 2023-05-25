ec55be185b83baa9c54b5bc156c1f376e7418740
Luikky <Luikky@outlokk.es>
2023-05-25 20:58:51 +0100

    Introduction to C#

---
+++ Consola Introduction/Consola Introduction/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Consola_Introduction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Presiona una Tecla para Salir...");
+            Console.ReadKey();
+        }
+    }
+}
