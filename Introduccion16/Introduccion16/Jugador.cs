9edc6921ed9ded2b0992b4f7541ccd36a8fc4b20
Luikky <Luikky@outlokk.es>
2023-06-05 20:16:43 +0100

    Programación Orientada a Objetos

---
+++ Introduccion16/Introduccion16/Jugador.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion16
+{
+    public class Jugador
+    {
+        public string nombre;
+        public int puntuacion;
+
+        public void SetPuntuacion(int puntos) 
+        {
+            puntuacion = puntuacion + puntos;            
+        }
+
+        public void ImprimirJugador() 
+        {
+            Console.WriteLine();
+            Console.Write("Nombre: " + nombre + " --> ");
+            Console.Write("Puntuación: " + puntuacion);        
+        }
+
+        public void Mensaje() // public virtual void Mensaje(); (Poliformismo son los numbres igual)  El padre no lo va Usar sino los hijos que los herederan
+        {
+            Console.WriteLine("\nSoy BATMAN\n");
+        }
+
+    }
+
+
+}
---
+++ Introduccion16/Introduccion16/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion16
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Jugador jugador = new Jugador();
+
+            jugador.nombre = "Pepe";
+            jugador.puntuacion = 0;
+
+            jugador.SetPuntuacion(20);
+            jugador.SetPuntuacion(10);
+            
+            jugador.Mensaje();
+            jugador.ImprimirJugador();
+
+            Perro perro = new Perro();
+            perro.nombre = "Toby";
+
+            perro.ImprimirJugador();
+            perro.Mensaje();
+
+            Gato gato = new Gato();
+            gato.nombre = "Garfield";
+
+            gato.ImprimirJugador();
+            gato.Mensaje();
+
+            Random random = new Random();
+            gato.SetPuntuacion(random.Next(10));
+            gato.ImprimirJugador();
+            Console.ReadKey();
+        }
+    }
+}
